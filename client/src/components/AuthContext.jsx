8dec45bb8170bfc65415d20a851cd4576044f061
Srikar M <140088299+code-srikar@users.noreply.github.com>
2024-07-08 17:36:23 +0530

    Add files via upload

--- client/src/components/AuthContext.jsx
+++ client/src/components/AuthContext.jsx
@@ -19,7 +19,7 @@ const AuthProvider = ({ children }) => {
     // Function to handle login
     const loginAction = async (data) => {
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('https://tasks-bc5v.onrender.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -50,7 +50,7 @@ const AuthProvider = ({ children }) => {
     // Function to handle signup
     const signupAction = async (data) => {
         try {
-            const response = await fetch('http://localhost:5000/api/signup', {
+            const response = await fetch('https://tasks-bc5v.onrender.com/api/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',

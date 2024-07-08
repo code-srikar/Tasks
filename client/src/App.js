45e7030f6946f02da19df6f7ddf8d17ab6bec5b0
Srikar M <140088299+code-srikar@users.noreply.github.com>
2024-07-08 17:06:36 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Tasks from './components/Tasks/Tasks';
+import AuthProvider from './components/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='*' element={<Home />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
